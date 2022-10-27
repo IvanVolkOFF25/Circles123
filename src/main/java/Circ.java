8426371d2a6526fe80b7e5ddad6360da46b3dc40
IvanVolkOFF25 <ivan.s.volkov@vsegda-da.com>
2022-10-28 00:49:06 +0300

    New commit

---
+++ src/main/java/Circ.java
@@ -0,0 +1,7 @@
+public class Circ {
+    public static void main(String[] args) {
+        double r = 7;
+        double output = (r * r) * 3.14;
+        System.out.println("Площадь круга равна: " + output);
+    }
+}
